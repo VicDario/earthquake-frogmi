c0b91b78dbe18bbe17de9b6e59b3b79520fa7ee8
VicDario <26585356+VicDario@users.noreply.github.com>
2024-04-14 12:42:46 -0400

    feat: add serialize method to return data with the correct format

--- app/models/earthquake.rb
+++ app/models/earthquake.rb
@@ -1,5 +1,4 @@
 class Earthquake < ApplicationRecord
-    self.inheritance_column = nil
     validates :magnitude, inclusion:
         { in: -1.0..10.0, message: "must be between -1.0 and 10.0" }
     validates :latitude, numericality:
@@ -8,4 +7,27 @@ class Earthquake < ApplicationRecord
         { greater_than_or_equal_to: -180.0, less_than_or_equal_to: 180.0, message: "must be between -180.0 and 180.0" }
 
     validates :title, :external_url, :place, :mag_type, :longitude, :latitude, presence: true
+
+    def serialize
+        {
+            id: id,
+            type: kind,
+            attributes: {
+                external_id: external_id,
+                magnitude: magnitude,
+                place: place,
+                time: time,
+                tsunami: tsunami,
+                mag_type: mag_type,
+                title: title,
+                coordinates: {
+                longitude: longitude,
+                latitude: latitude
+                }
+            },
+            links: {
+                external_url: external_url
+            }
+        }
+    end
 end
