3f031cfaebb99c01c432f51a46f0e2bf91715af3
VicDario <26585356+VicDario@users.noreply.github.com>
2024-04-14 17:34:12 -0400

    test: test for comment model

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,7 +1,22 @@
 require "test_helper"
 
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "create valid record successfully" do
+    comment = Comment.new(
+      earthquake_id: 1,
+      body: "This is a comment"
+    )
+    assert_instance_of Comment, comment, "Record is not an instance of Comment"
+    assert comment.valid?, "Record was not valid. Errors: #{comment.errors.full_messages.join(", ")}"
+  end
+
+  test "Body cannot be blank" do
+    comment = Comment.new(
+      earthquake_id: 1,
+      body: ""
+    )
+    assert_not comment.valid?, "Record was valid"
+    assert_includes comment.errors.full_messages, "Body can't be blank", "Errors did not match"
+    assert_includes comment.errors.full_messages, "Body is too short (minimum is 1 character)", "Errors did not match"
+  end
 end
