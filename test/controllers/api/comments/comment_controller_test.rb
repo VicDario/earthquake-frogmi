f10ab598819e25dc2f4d8a17d1e77227e223ee47
VicDario <26585356+VicDario@users.noreply.github.com>
2024-04-14 17:34:33 -0400

    test: test for comment controller

--- test/controllers/api/comments/comment_controller_test.rb
+++ test/controllers/api/comments/comment_controller_test.rb
@@ -1,4 +1,13 @@
 require "test_helper"
 
 class CommentControllerTest < ActionDispatch::IntegrationTest
+  test "create valid comment" do
+    post '/api/comments', params: { comment: { body: "This is a comment", earthquake_id: 1 } }
+    assert_response :created
+  end
+
+  test "Error when comment with empty body" do
+    post '/api/comments', params: { comment: { body: "", earthquake_id: 1 } }
+    assert_response :unprocessable_entity
+  end
 end
