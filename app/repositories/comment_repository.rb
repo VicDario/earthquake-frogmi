194b0451a27f797453ffc0bcd8a8062059443cb8
VicDario <26585356+VicDario@users.noreply.github.com>
2024-04-14 22:06:56 -0400

    feat: method to get comments by earthquake

--- app/repositories/comment_repository.rb
+++ app/repositories/comment_repository.rb
@@ -2,4 +2,9 @@ class CommentRepository
     def self.save(comment_data)
         Comment.create(comment_data)
     end
+
+    def self.get_by_earthquake_id(earthquake_id)
+        comments = Comment.where(earthquake_id: earthquake_id)
+        comments.map(&:serialize)
+    end
 end
