d8ee927d25b523e53a72f40ebe390bbddf30ed1f
VicDario <26585356+VicDario@users.noreply.github.com>
2024-04-15 16:46:31 -0400

    chore: delete unused imports

--- app/javascript/routes/index.jsx
+++ app/javascript/routes/index.jsx
@@ -1,22 +1,15 @@
 import React from 'react';
-import {
-    createBrowserRouter,
-    RouterProvider,
-    BrowserRouter as Router,
-    Routes,
-    Route
-} from "react-router-dom";
+import { createBrowserRouter } from 'react-router-dom';
 import EarthquakeList from '../components/earthquake-list.component';
 import EarthquakeDetails from '../components/earthquake-details.component';
 
 export const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <EarthquakeList />,
-    },
-    {
-        path: "/earthquake/:id",
-        element: <EarthquakeDetails />,
-    
-    }
+  {
+    path: '/',
+    element: <EarthquakeList />,
+  },
+  {
+    path: '/earthquake/:id',
+    element: <EarthquakeDetails />,
+  },
 ]);
