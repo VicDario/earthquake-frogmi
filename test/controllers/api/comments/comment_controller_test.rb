b66d0639b7859f2af2af4238b3d47701ea08db23
VicDario <26585356+VicDario@users.noreply.github.com>
2024-04-15 00:16:28 -0400

    test: update params for new required payload

--- test/controllers/api/comments/comment_controller_test.rb
+++ test/controllers/api/comments/comment_controller_test.rb
@@ -2,12 +2,12 @@ require "test_helper"
 
 class CommentControllerTest < ActionDispatch::IntegrationTest
   test "create valid comment" do
-    post '/api/comments', params: { comment: { body: "This is a comment", earthquake_id: 1 } }
+    post '/api/comments', params: { body: "This is a comment", feature_id: 1 }
     assert_response :created
   end
 
   test "Error when comment with empty body" do
-    post '/api/comments', params: { comment: { body: "", earthquake_id: 1 } }
+    post '/api/comments', params: { body: "", feature_id: 1  }
     assert_response :unprocessable_entity
   end
 end
