04cb425ca613e0921a2b745585a4387924a0b123
VicDario <26585356+VicDario@users.noreply.github.com>
2024-04-14 23:52:05 -0400

    fix: feature_id for comments

--- app/controllers/api/comments/comment_controller.rb
+++ app/controllers/api/comments/comment_controller.rb
@@ -2,14 +2,14 @@ class Api::Comments::CommentController < ApplicationController
   skip_before_action :verify_authenticity_token # Disable CSRF protection for API, Only for development purpose
 
   def index
-    earthquake_id = params[:earthquake_id]
+    feature_id = params[:feature_id]
     
-    if earthquake_id.nil?
-      render json: { error: 'Earthquake ID is required' }, status: :bad_request
+    if feature_id.nil?
+      render json: { error: 'Feature ID is required' }, status: :bad_request
       return
     end
 
-    comments = CommentRepository.get_by_earthquake_id(earthquake_id)
+    comments = CommentRepository.get_by_earthquake_id(feature_id)
     render json: comments
   end
 
@@ -24,6 +24,10 @@ class Api::Comments::CommentController < ApplicationController
 
   private
   def comment_payload
-    params.require(:comment).permit(:body, :earthquake_id)
+    payload = {
+      earthquake_id: params[:feature_id],
+      body: params[:body],
+    }
+    payload
   end
 end
--- app/repositories/comment_repository.rb
+++ app/repositories/comment_repository.rb
@@ -1,10 +1,11 @@
 class CommentRepository
     def self.save(comment_data)
+        puts comment_data
         Comment.create(comment_data)
     end
 
-    def self.get_by_earthquake_id(earthquake_id)
-        comments = Comment.where(earthquake_id: earthquake_id)
+    def self.get_by_earthquake_id(feature_id)
+        comments = Comment.where(earthquake_id: feature_id)
         comments.map(&:serialize)
     end
 end
