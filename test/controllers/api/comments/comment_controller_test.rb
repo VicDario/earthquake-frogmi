3bf122f6d0c7d9826fea22a11404394c7815bfb7
VicDario <26585356+VicDario@users.noreply.github.com>
2024-04-15 16:40:32 -0400

    test: add test for comment controller

--- test/controllers/api/comments/comment_controller_test.rb
+++ test/controllers/api/comments/comment_controller_test.rb
@@ -1,13 +1,28 @@
 require "test_helper"
 
 class CommentControllerTest < ActionDispatch::IntegrationTest
-  test "create valid comment" do
-    post '/api/comments', params: { body: "This is a comment", feature_id: 1 }
+  def setup
+    @feature_id = 1
+  end
+
+  test "Get comments by feature id" do
+    get '/api/comments', params: { feature_id: @feature_id }
+    assert_response :success
+  end
+
+  test "Should return bad request if feature_id is missing" do
+    get '/api/comments'
+    assert_response :bad_request
+    assert_equal({ "error" => "Feature ID is required" }, JSON.parse(response.body))
+  end
+
+  test "Create valid comment" do
+    post '/api/comments', params: { comment: { body: 'This is a comment', feature_id: @feature_id } }
     assert_response :created
   end
 
   test "Error when comment with empty body" do
-    post '/api/comments', params: { body: "", feature_id: 1  }
+    post '/api/comments', params: { comment: { body: '', feature_id: @feature_id } }
     assert_response :unprocessable_entity
   end
 end
