776ea65907b34b92c4a34575b85849793a6c9485
VicDario <26585356+VicDario@users.noreply.github.com>
2024-04-14 22:06:37 -0400

    feat: comment serialize method

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,11 @@ class Comment < ApplicationRecord
   belongs_to :earthquake
 
   validates :body, presence: true, length: { minimum: 1 }
+
+  def serialize
+    {
+      body: body,
+      created_at: created_at,
+    }
+  end
 end
