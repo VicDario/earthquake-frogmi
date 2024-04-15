72c29b4588e4e3df282e1d6d7f72b83cb3db9d56
VicDario <26585356+VicDario@users.noreply.github.com>
2024-04-15 16:40:57 -0400

    chore: update routes for features

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,11 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   namespace :api do
-    namespace :earthquakes do
-      get "/" => "earthquake#index", as: :earthquake_data
+    namespace :features do
+      get "/" => "feature#index", as: :get_features
     end
     namespace :comments do
-      get "/" => "comment#index", as: :comment_by_earthquake
+      get "/" => "comment#index", as: :comment_by_feature
       post "/" => "comment#create", as: :save_comment
     end
   end
